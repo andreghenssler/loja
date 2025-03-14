934db84a2aaedc98671b38729bd37939f8d81150
Andre Gustavo Henssler <38543213+andreghenssler@users.noreply.github.com>
2025-03-13 22:03:19 -0300

    Atualizado o nome
    Mudado o nome

--- banco.sql
+++ banco.sql
@@ -1,26 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.9.7
--- https://www.phpmyadmin.net/
---
+
 -- Host: localhost:3306
 -- Tempo de geração: 13/01/2022 às 15:22
 -- Versão do servidor: 5.7.23-23
 -- Versão do PHP: 7.3.32
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `henssl03_loja`
---
 
 -- --------------------------------------------------------
 
